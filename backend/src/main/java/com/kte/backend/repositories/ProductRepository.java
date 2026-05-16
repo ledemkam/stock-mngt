b8a1b452f0aae58144e333aa45d474a0a73fb84a
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-05-16 08:36:24 +0200

    feat(backend): add findByNameIgnoreCase method to ProductRepository

--- backend/src/main/java/com/kte/backend/repositories/ProductRepository.java
+++ backend/src/main/java/com/kte/backend/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.kte.backend.repositories;
 import com.kte.backend.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByNameIgnoreCase(String name);
 }
