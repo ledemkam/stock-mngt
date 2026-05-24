73fc641d072a3bc78726ad8cf9732eccc91c3fb3
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-05-24 20:25:39 +0200

    feat(backend): implement UserServiceImpl with username-based user loading

---
+++ backend/src/main/java/com/kte/backend/services/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.kte.backend.services.impl;
+
+import com.kte.backend.repositories.UserRepository;
+import com.kte.backend.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private  final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
