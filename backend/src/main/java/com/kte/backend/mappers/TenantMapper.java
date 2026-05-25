891b6060cb553736d061da8e417728b3c4cab110
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-05-25 11:01:30 +0200

    feat(backend): add TenantMapper for tenant registration request/response mapping

---
+++ backend/src/main/java/com/kte/backend/mappers/TenantMapper.java
@@ -0,0 +1,37 @@
+package com.kte.backend.mappers;
+
+import com.kte.backend.dto.requests.RegisterTenantRequest;
+import com.kte.backend.dto.responses.TenantResponse;
+import com.kte.backend.entities.Tenant;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TenantMapper {
+
+    public Tenant toEntity(final RegisterTenantRequest request){
+        return Tenant.builder()
+                .compagnyName(request.getCompagnyName())
+                .compagnyCode(request.getCompagnyCode())
+                .createdAt(LocalDateTime.now())
+                .email(request.getEmail())
+                .adminFullName(request.getAdminFullName())
+                .adminUserName(request.getAdminUserName())
+                //.adminPassword(request.getAdminPassword()) ->here can ignored,direct implement in service
+                .adminEmail(request.getAdminEmail())
+                .build();
+    }
+
+    public TenantResponse toResponse(final Tenant entity){
+        return TenantResponse.builder()
+                .compagnyName(entity.getCompagnyName())
+                .compagnyCode(entity.getCompagnyCode())
+                .email(entity.getEmail())
+                .adminFullName(entity.getAdminFullName())
+                .adminUserName(entity.getAdminUserName())
+                .adminEmail(entity.getAdminEmail())
+                .status(entity.getStatus())
+                .build();
+    }
+}
