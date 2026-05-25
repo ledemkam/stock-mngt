6f4a7ef96c7196686c03119fda4bf33d58df25d0
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-05-25 22:34:29 +0200

    feat(backend): expand UserService interface with user management methods

--- backend/src/main/java/com/kte/backend/services/auth/UserService.java
+++ backend/src/main/java/com/kte/backend/services/auth/UserService.java
@@ -1,8 +1,25 @@
 package com.kte.backend.services.auth;
 
+import com.kte.backend.common.PageReponse;
+import com.kte.backend.dto.requests.UserRequest;
+import com.kte.backend.dto.responses.UserResponse;
 import com.kte.backend.entities.Tenant;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-    void createAdminUser(Tenant tenant);
+    void createAdminUser(final Tenant tenant);
+
+    void createUser(final String userId, final UserRequest request);
+
+     void updateUser(final String userId, final UserRequest request);
+
+     void deleteUser(final String userId);
+
+     UserResponse getUserById(final String userId);
+
+     PageReponse<UserResponse> getAllUsers(final int page, final int size);
+
+     void enableUser(final  String userId);
+
+     void disableUser(final String userId);
 }
