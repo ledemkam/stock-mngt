c10800867d171c4f7fd23a7a3a16e31577527546
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-05-25 10:57:55 +0200

    feat(backend): add BCryptPasswordEncoder bean for password encryption

--- backend/src/main/java/com/kte/backend/config/BeansConfigs.java
+++ backend/src/main/java/com/kte/backend/config/BeansConfigs.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeansConfigs {
@@ -11,5 +13,10 @@ public class BeansConfigs {
     @Bean
     public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
 }
---
+++ backend/src/main/java/com/kte/backend/exceptions/DuplicateEntityException.java
@@ -0,0 +1,21 @@
+package com.kte.backend.exceptions;
+
+public class DuplicateEntityException extends RuntimeException {
+
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
+
+    public DuplicateEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateEntityException(Throwable cause) {
+        super(cause);
+    }
+
+    public DuplicateEntityException(String message, Throwable cause,
+                                 boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
