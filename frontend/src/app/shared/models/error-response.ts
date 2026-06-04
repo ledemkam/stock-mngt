c87826cd9c8a2cba58484a3ea65a138a7630ed8f
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-06-04 23:14:11 +0200

    removed_frontend

--- frontend/src/app/shared/models/error-response.ts
+++
@@ -1,8 +0,0 @@
-import {ValidationError} from './validation-error';
-
-export interface ErrorResponse {
-  code?: string;
-  message?: string;
-  path?: string;
-  validationErrors?: Array<ValidationError>;
-}
