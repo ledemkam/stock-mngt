15c902ff56cd343200485a01bd06ebc1d6e09085
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-06-13 10:44:47 +0200

    feat: add security scheme configuration for JWT in Swagger

--- src/main/java/com/kte/backend/config/SwaggerConfig.java
+++ src/main/java/com/kte/backend/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.kte.backend.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
@@ -7,6 +10,13 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@SecurityRequirement(name = "bearerAuth")
 @Configuration
 public class SwaggerConfig {
     //http://localhost:8080/swagger-ui/index.html
